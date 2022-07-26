1be26e2c2908ab217c4aec1491a73885bfeafee4
NamHoai1210 <ch.nam0121@gmail.com>
2022-07-26 16:07:46 +0700

    Update Database

--- admin/pages/process_add_movie.php
+++ admin/pages/process_add_movie.php
@@ -5,7 +5,7 @@ extract($_POST);
 $target_dir = "../../images/";
 $target_file = $target_dir . basename($_FILES["image"]["name"]);
 $flname = "images/" . basename($_FILES["image"]["name"]);
-mysqli_query($con, "INSERT INTO  tbl_movie VALUES(NULL,'$name','$cast','$desc','$rdate','$flname','$video','$length')");
+mysqli_query($con, "INSERT INTO  tbl_movie VALUES(NULL,'$name','$cast','$desc','$rdate','$flname','$video','$length',0,0)");
 move_uploaded_file($_FILES["image"]["tmp_name"], $target_file);
 echo $_FILES["image"]["tmp_name"];
 $_SESSION['success'] = "Movie Added";
